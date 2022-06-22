9985f35f2a8d1887ea2e0bc75252f6c5539d19ac
jakewmiles <jake.w.miles@gmail.com>
2022-06-22 23:00:47 +0100

    write tests for moving a robot

--- test/toy_robot/robot_test.exs
+++ test/toy_robot/robot_test.exs
@@ -16,5 +16,51 @@ defmodule ToyRobot.RobotTest do
     end
   end
 
+  describe "move/1" do
+    test "moves a robot forward 1 space in a given direction" do
+      robot = setup_test_robot()
+
+      post_move_robot = Robot.move(robot)
+
+      assert post_move_robot.north == 1
+      assert post_move_robot.east == 0
+      assert post_move_robot.facing == :north
+    end
+
+    test "multiple moves can be chained together" do
+      robot = setup_test_robot()
+
+      post_move_robot =
+        robot
+        |> Robot.move()
+        |> Robot.move()
+        |> Robot.move()
+        |> Robot.move()
+        |> Robot.move()
+
+      assert post_move_robot.north == 5
+      assert post_move_robot.east == 0
+      assert post_move_robot.facing == :north
+    end
+
+    test "robot can't move beyond the end of the board" do
+      robot = setup_test_robot()
+
+      post_move_robot =
+        robot
+        |> Robot.move()
+        |> Robot.move()
+        |> Robot.move()
+        |> Robot.move()
+        |> Robot.move()
+
+      assert post_move_robot.north == 5
+
+      end_of_board_robot = Robot.move(post_move_robot)
+
+      assert end_of_board_robot.north == 5
+    end
+  end
+
   defp setup_test_robot(), do: %Robot{north: 0, east: 0, facing: :north}
 end

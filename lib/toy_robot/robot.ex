98b15f638752b68a1f0e8638f2bda4890fd7e6b0
jakewmiles <jake.w.miles@gmail.com>
2022-06-22 21:30:13 +0100

    implement move_west

--- lib/toy_robot/robot.ex
+++ lib/toy_robot/robot.ex
@@ -16,4 +16,22 @@ defmodule ToyRobot.Robot do
   def move_east(%{position: current_position}) do
     %{position: current_position + 1}
   end
+
+  @doc """
+  Moves the robot east one space.
+
+  ## Examples
+
+    iex> alias ToyRobot.Robot
+    ToyRobot.Robot
+    iex> robot = %{position: 0}
+    %{position: 0}
+    iex> robot |> Robot.move_west
+    %{position: -1}
+    iex> robot |> Robot.move_west |> Robot.move_west |> Robot.move_west
+    %{position: -3}
+  """
+  def move_west(%{position: current_position}) do
+    %{position: current_position - 1}
+  end
 end
